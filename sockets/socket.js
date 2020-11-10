9a8f8eabc5fac2999af4f8059151f07dd013fc3a
Christopher Leal <cleal@dbmenos.com>
2020-11-10 11:36:00 -0600

    creacion de los modelos y metodos para el crud de las bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,21 @@
 const { io } = require('./../server');
+
+const Bands = require('./../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands()
+
+bands.addBand(new Band('Queen'))
+bands.addBand(new Band('Queen 2'))
+bands.addBand(new Band('Queen 3'))
+bands.addBand(new Band('Queen 4'))
+
 // mensajes de sockets
 io.on('connection', (client) => {
     console.log('cliente conectado');
+
+    client.emit('active-bands', bands.getBands())
+
     client.on('disconnect', () => {
         console.log('cliente desconectado');
     });
@@ -10,6 +24,33 @@ io.on('connection', (client) => {
         console.log(payload);
 
         io.emit('message', { admin: 'Nuevo mensaje' })
+    });
+
+    client.on('new-message', (payload) => {
+        console.log(payload);
+
+        client.broadcast.emit('new-message', payload)
+    });
+
+    client.on('vote-for-a-band', (payload) => {
+        console.log(payload);
+        bands.voteBand(payload.id)
+        io.emit('active-bands', bands.getBands())
+
+    });
+
+    client.on('add-band', (payload) => {
+        console.log(payload);
+        bands.addBand(new Band(payload.name))
+        io.emit('active-bands', bands.getBands())
+
+    });
+
+    client.on('delete-band', (payload) => {
+        console.log(payload);
+        bands.deleteBand(payload.id)
+        io.emit('active-bands', bands.getBands())
+
     });
 
 });
